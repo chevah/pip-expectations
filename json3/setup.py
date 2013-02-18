4c468727e56fb3753a312f78107dca660e83bf21
Adi Roiban <adi.roiban@chevah.com>
2013-02-18 22:57:59 +0200

    Add json3.

---
+++ json3/setup.py
@@ -0,0 +1,107 @@
+"""
+Python packaging definition.
+
+It downloads the JS and CSS Files from upstream website
+and creates a package.
+"""
+
+from distutils import log
+from setuptools import setup, Command
+import os
+import shutil
+
+NAME = 'chevah-weblibs-json3'
+VERSION = '3.2.4'
+CHEVAH_VERSION = '-chevah1'
+DOWNLOADS = [
+    ('http://cdnjs.cloudflare.com/ajax/libs/json3/%(version)s/json3.min.js',
+        'chevah/weblibs/json3/json3.min.js'),
+    ]
+
+
+def download():
+    """
+    Download file.
+    """
+    import urllib2
+    for remote, local in DOWNLOADS:
+        url = remote % {'version': VERSION}
+        mp3file = urllib2.urlopen(url)
+        output = open(local, 'wb')
+        output.write(mp3file.read())
+        output.close()
+
+
+class PublishCommand(Command):
+    """
+    Publish the source distribution to local pypi cache and remote
+    Chevah PyPi server.
+    """
+
+    description = "copy distributable to Chevah cache folder"
+    user_options = []
+
+    def initialize_options(self):
+        self.cwd = None
+        self.destination_base = '~/chevah/brink/cache/pypi/'
+
+    def finalize_options(self):
+        self.cwd = os.getcwd()
+
+    def run(self):
+        assert os.getcwd() == self.cwd, (
+            'Must be in package root: %s' % self.cwd)
+        download()
+        self.run_command('sdist')
+        sdist_command = self.distribution.get_command_obj('sdist')
+        for archive in sdist_command.archive_files:
+            source = os.path.join(archive)
+            destination = os.path.expanduser(
+                self.destination_base + os.path.basename(archive))
+            shutil.copyfile(source, destination)
+        log.info(
+            "Distributables files copied to %s " % (self.destination_base))
+
+        # Upload package to Chevah PyPi server.
+        upload_command = self.distribution.get_command_obj('upload')
+        upload_command.repository = u'chevah'
+        self.run_command('upload')
+
+        # Delete temporary file downloaded only for building the package.
+        for remote, local in DOWNLOADS:
+            os.remove(local)
+
+
+def find_package_data(modules):
+    """
+    Returns the static dictionary of data files.
+
+    TODO: add dynamic code.
+    """
+    result = {}
+    for module in modules:
+        result.update({
+            module: [
+                '*.js',
+                '*.css',
+                ]})
+    return result
+
+
+setup(
+    name=NAME,
+    version=VERSION + CHEVAH_VERSION,
+    maintainer="Adi Roiban",
+    maintainer_email="adi.roiban@chevah.com",
+    license='Same as json3',
+    platforms='any',
+    description='Files for json3 used in Chevah project.',
+    long_description=open('README.rst').read(),
+    url='http://bestiejs.github.com/json3/',
+    namespace_packages=['chevah', 'chevah.weblibs'],
+    packages=['chevah', 'chevah.weblibs', 'chevah.weblibs.json3'],
+    package_data=find_package_data(['chevah.weblibs.json3']),
+    cmdclass={
+        'publish': PublishCommand,
+        },
+    )
