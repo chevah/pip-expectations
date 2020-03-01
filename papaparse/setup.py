a2a1d8976306799227ead140ad92b3c3efd037c1
Adi Roiban <adi.roiban@chevah.com>
2020-03-01 11:41:21 +0000

    Add papaparse.

---
+++ papaparse/setup.py
@@ -0,0 +1,120 @@
+from setuptools import setup, Command
+import os
+
+NAME = 'chevah-weblibs-papaparse'
+MODULE_NAME = 'papaparse'
+VERSION = '59e475e'
+CHEVAH_VERSION = '.chevah1'
+WEBSITE = 'http://papaparse.com/'
+AUTHOR = 'Papa Parse Team'
+LICENSE = 'MIT'
+
+BASE_URL = (
+    # Upstream
+    #'https://raw.githubusercontent.com/chevah/PapaParse/%(version)s/'
+    'https://raw.githubusercontent.com/chevah/PapaParse/%(version)s/'
+    )
+BASE_PATH = 'chevah/weblibs/%s/' % (MODULE_NAME)
+FILES = [
+    ('', 'papaparse.js'),
+    ]
+
+
+def add_version(name):
+    if name.endswith('.min.js'):
+        return name[:-7] + '-' + VERSION + '.min.js'
+    if name.endswith('.js'):
+        return name[:-3] + '-' + VERSION + '.js'
+    if name.endswith('.min.css'):
+        return name[:-8] + '-' + VERSION + '.min.css'
+    if name.endswith('.css'):
+        return name[:-4] + '-' + VERSION + '.css'
+    return name
+
+DOWNLOADS = []
+for (remote_path, filename) in FILES:
+    remote = (BASE_URL + remote_path + filename) % {'version': VERSION}
+    local = add_version(BASE_PATH + filename)
+    DOWNLOADS.append((remote, local))
+
+
+def download():
+    """
+    Download files.
+    """
+    import urllib2
+    for remote, local in DOWNLOADS:
+        print "Getting %s into %s" % (remote, local)
+        mp3file = urllib2.urlopen(remote)
+        output = open(local, 'wb')
+        output.write(mp3file.read())
+        output.close()
+
+
+class PublishCommand(Command):
+    """
+    Publish the source distribution to local pypi cache and remote
+    Chevah PyPi server.
+    """
+    user_options = []
+
+    def initialize_options(self):
+        self.cwd = None
+
+    def finalize_options(self):
+        self.cwd = os.getcwd()
+
+    def run(self):
+        assert os.getcwd() == self.cwd, (
+            'Must be in package root: %s' % self.cwd)
+        download()
+        self.run_command('bdist_wheel')
+
+        # Upload package to Chevah PyPi server.
+        upload_command = self.distribution.get_command_obj('upload')
+        upload_command.repository = u'chevah'
+        self.run_command('upload')
+
+        # Delete temporary files downloaded only for building the package.
+        for remote, local in DOWNLOADS:
+            os.remove(local)
+
+
+def find_package_data(modules):
+    """
+    Returns the static dictionary of data files.
+
+    TODO: add dynamic code.
+    """
+    result = {}
+    for module in modules:
+        result.update({
+            module: [
+                '*.js',
+                '*.css',
+                '*.png',
+                '*.gif',
+                '*.jpg',
+                ]})
+    return result
+
+
+setup(
+    name=NAME,
+    version=VERSION + CHEVAH_VERSION,
+    author=AUTHOR,
+    author_email='hidden',
+    maintainer="Adi Roiban",
+    maintainer_email="adi.roiban@chevah.com",
+    license=LICENSE,
+    platforms='any',
+    description='Files for %s used in Chevah project.' % (MODULE_NAME),
+    long_description=open('README.rst').read(),
+    url=WEBSITE,
+    namespace_packages=['chevah', 'chevah.weblibs'],
+    packages=['chevah', 'chevah.weblibs', 'chevah.weblibs.' + MODULE_NAME],
+    package_data=find_package_data(['chevah.weblibs.' + MODULE_NAME]),
+    cmdclass={
+        'publish': PublishCommand,
+        },
+    )
