5eab5caba77bac1f4c6cea786964fdd69fd56e37
Adi Roiban <adi.roiban@chevah.com>
2024-06-10 20:03:39 +0100

    Update shawl.

--- shawl/README.rst
+++ shawl/README.rst
@@ -2,3 +2,5 @@ shawl
 =====
 
 Python package for shawl files used in Chevah Project.
+
+Manual download the latest shawl.exe and copy it to ./chevah_shawl/.
--- shawl/setup.py
+++ shawl/setup.py
@@ -3,28 +3,14 @@
 # https://github.com/mtkennerly/shawl/releases
 # Then copy the file as shawl.exe inside chevah/shawl
 from setuptools import setup, Command
-import os
 
 NAME = 'chevah-shawl'
-MODULE_NAME = 'shawl'
-VERSION = '1.2.0'
+MODULE_NAME = 'chevah_shawl'
+VERSION = '1.5.0'
 CHEVAH_VERSION = '+chevah.1'
 WEBSITE = 'https://github.com/mtkennerly/shawl'
 AUTHOR = 'Matthew T. Kennerly'
 LICENSE = 'MIT'
-
-
-def find_package_data(modules):
-    """
-    Returns the static dictionary of data files.
-    """
-    result = {}
-    for module in modules:
-        result.update({
-            module: ['*.exe']})
-    return result
-
-package_name = 'chevah.' + MODULE_NAME
 
 
 setup(
@@ -36,11 +22,10 @@ setup(
     maintainer_email="adi.roiban@chevah.com",
     license=LICENSE,
     platforms='any',
-    description='Files for %s used in Chevah project.' % (MODULE_NAME),
+    description='Files for %s used in Chevah project.'.format(MODULE_NAME),
     long_description=open('README.rst').read(),
     url=WEBSITE,
-    namespace_packages=['chevah'],
-    packages=['chevah', package_name],
-    package_dir={package_name: 'chevah/' + MODULE_NAME},
-    package_data={package_name: ['*']},
+    packages=[MODULE_NAME],
+    package_dir={MODULE_NAME: MODULE_NAME},
+    package_data={MODULE_NAME: ['*']},
     )
